a55faeb3cdf87a02eaa308492377da4a53fc4aee
Lex Le <sonle@outlook.com.au>
2026-03-18 00:42:44 +1100

    first blog Vietnamese

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Trang chủ' },
+  { href: '/blog', title: 'Bài viết' },
+  // { href: '/projects', title: 'Dự án' }, // TODO: bật lại khi có series toán
+  { href: '/tags', title: 'Chủ đề' },
+  { href: '/about', title: 'Giới thiệu' },
+]
+
+export default headerNavLinks
